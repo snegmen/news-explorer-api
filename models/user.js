113e9a776a8d9ca61d8537bf15feec84c164ccc5
snegmen <che1376@mail.ru>
2019-11-15 13:24:50 +0700

    Созданы модели и схемы

--- models/user.js
+++ models/user.js
@@ -24,32 +24,18 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
   },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validate.isURL(v),
-      message: (props) => `${props.value} Неверный URL!`,
-    },
-  },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        throw Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new Error('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            throw Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new Error('Неправильные почта или пароль'));
           }
           return user;
         });
